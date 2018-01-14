1e31fbdcf02be203765c7e1b23128bb02ad1e9ba
Grigory Rylov <grishberg@gmail.com>
2018-01-14 18:02:55 +0300

    initial commit

---
+++ distance_measurer.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class DistanceMeasurer:
+    def __init__(self, trig, echo):
+        self._running = True
+        self.min_dist = 10
+        self.interval = 0.100
+        self.distance = 0.
+        self._trig = trig
+        self._echo = echo
+        self._thread = None
+        self._measure_listener = None
+
+        print "Distance Measurement In Progress"
+        GPIO.setup(self._trig, GPIO.OUT)
+        GPIO.setup(self._echo, GPIO.IN)
+
+        GPIO.output(self._trig, False)
+
+    def measure(self):
+        pulse_start = 0
+        pulse_end = 17150 * 999
+
+        GPIO.output(self._trig, True)
+        time.sleep(0.00001)
+        GPIO.output(self._trig, False)
+
+        while GPIO.input(self._echo) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(self._echo) == 1:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        return round(pulse_duration * 17150, 2)
+
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+
+    d = DistanceMeasurer(4, 17)
+    d.measure()
+
+    GPIO.cleanup()
---
+++ servo_observer.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import threading
+from pca9685 import *
+from distance_measurer import DistanceMeasurer
+
+STEP = 3
+
+ECHO = 17
+TRIG = 4
+DEBUG = True
+
+
+class ServoObserver:
+    def __init__(self, channel):
+        self._run = False
+        self._servo = PCA9685()
+        self._measurer = DistanceMeasurer(TRIG, ECHO)
+        self._channel = channel
+        self._thread = None
+        self._min_range = 0
+        self._min_range_listener = None
+
+        self._servo.servos[self._channel].set(signed=True, reverse=True,
+                                              min=100, max=100,
+                                              trim=0, exp=0)
+        self._servo.setServo(self._channel, 0)
+        time.sleep(1)
+
+    def start(self, min_range, min_range_listener):
+        self._run = True
+        # init threads
+        self._min_range = min_range
+        self._min_range_listener = min_range_listener
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.start()
+
+    def stop(self):
+        self._run = False
+
+    def cleanup(self):
+        self._servo.off()
+
+    def _run(self):
+        last_distance_greater = 999
+        start_range = -100
+        end_range = 100
+        while self._run:
+            distance_array = {}
+            min_distance = 999
+            min_angle = 0
+
+            for a in xrange(start_range, end_range, STEP):
+                # turn servo into angle
+                self._servo.setServo(self._channel, a)
+                time.sleep(0.01)
+                # measure distance
+                distance = 999
+                for i in range(10):
+                    distance = self._measurer.measure()
+                    if distance < 999:
+                        break
+                if DEBUG:
+                    print ('{} distance is {}'.format(a, distance))
+                if min_distance > distance:
+                    distance = min_distance
+                    min_angle = a
+                # store distance for angle
+                distance_array[a] = distance
+
+            # swap ranges
+            start_range, end_range = end_range, start_range
+            if min_distance < self._min_range:
+                if last_distance_greater and self._min_range_listener is not None:
+                    self._min_range_listener(distance_array)
+                    last_distance_greater = False
+            else:
+                last_distance_greater = True
+
+
+if __name__ == '__main__':
+    o = ServoObserver(0)
+
+    o.start(10, None)
+    time.sleep(10)
+    o.stop()
