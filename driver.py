a9ef5b3d8c89d075b0c5d91a657eef1ba4ed7de8
Grigory Rylov <grishberg@gmail.com>
2018-01-23 08:53:27 +0300

    added websocket server

---
+++ driver.py
@@ -0,0 +1,68 @@
+import RPi.GPIO as gpio
+import time
+
+
+# from ultrasonic import distance
+
+class Driver:
+    def __init__(self):
+        pass
+
+    def init(self):
+        gpio.setmode(gpio.BCM)
+        gpio.setup(27, gpio.OUT)
+        gpio.setup(22, gpio.OUT)
+        gpio.setup(23, gpio.OUT)
+        gpio.setup(24, gpio.OUT)
+
+    def wait_and_clean(self, t):
+        time.sleep(t)
+        # gpio.cleanup()
+
+    def release(self):
+        gpio.output(27, False)
+        gpio.output(22, False)
+        gpio.output(23, False)
+        gpio.output(24, False)
+
+    def forward(self, tf):
+        self.init()
+        gpio.output(27, True)
+        gpio.output(22, False)
+        gpio.output(23, True)
+        gpio.output(24, False)
+        self.wait_and_clean(tf)
+
+    def reverse(self, tf):
+        self.init()
+        gpio.output(27, False)
+        gpio.output(22, True)
+        gpio.output(23, False)
+        gpio.output(24, True)
+        self.wait_and_clean(tf)
+
+    def right(self, t):
+        self.init()
+        gpio.output(27, True)
+        gpio.output(22, False)
+        gpio.output(23, False)
+        gpio.output(24, True)
+        self.wait_and_clean(t)
+
+    def left(self, t):
+        self.init()
+        gpio.output(27, False)
+        gpio.output(22, True)
+        gpio.output(23, True)
+        gpio.output(24, False)
+        self.wait_and_clean(t)
+
+    def turn_left(self):
+        self.left(0.85)
+
+    def turn_right(self):
+        self.right(0.85)
+
+    @staticmethod
+    def clean():
+        gpio.cleanup()
---
+++ remote_control_server.py
@@ -0,0 +1,67 @@
+from tornado import websocket
+import tornado.ioloop
+from driver import Driver
+
+UP = 'u'
+DOWN = 'd'
+LEFT = 'l'
+RIGHT = 'r'
+TURN_LEFT = '<'
+TURN_RIGHT = '>'
+
+PRESSED = 'p'
+RELEASED = 'r'
+
+QUIT = 'q'
+
+
+class EchoWebSocket(websocket.WebSocketHandler):
+    def __init__(self, application, request, **kwargs):
+        super(EchoWebSocket, self).__init__(application, request, **kwargs)
+        self._car_controller = Driver()
+        self._mode = None
+
+    def open(self):
+        print "Websocket Opened"
+        self._car_controller.init()
+
+    def on_message(self, message):
+        if len(message) != 2:
+            print "wrong command length: %s" % message
+        cmd = message[0]
+        action = message[1]
+
+        self.process_cmd(cmd, action)
+
+        self.write_message(u"You said: %s" % message)
+
+    def on_close(self):
+        print "Websocket closed"
+        self._car_controller.clean()
+
+    def process_cmd(self, cmd, action):
+        if cmd == QUIT:
+            self.quit()
+            return
+        if action == PRESSED:
+            if cmd == UP:
+                self._car_controller.forward(0.5)
+            elif cmd == DOWN:
+                self._car_controller.reverse(0.5)
+            elif cmd == LEFT:
+                self._car_controller.left(0.5)
+            elif cmd == RIGHT:
+                self._car_controller.right(0.5)
+        elif action == RELEASED:
+            self._car_controller.release()
+
+    def quit(self):
+        self.write_message("quit")
+
+
+
+application = tornado.web.Application([(r"/", EchoWebSocket), ])
+
+if __name__ == "__main__":
+    application.listen(9000)
+    tornado.ioloop.IOLoop.instance().start()
